a3df877f75054277113648c83edeb861be3d4760
sparkydeal <97933363+sparkydeal@users.noreply.github.com>
2023-10-05 09:07:27 +0300

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,28 @@
+let c = (...args) => console.log(...args); 
+
+let course = {
+    title: 'Learn CSS Grid for free',
+    lesson: 16,
+    length: 60,
+    creator: 'Per Betty Borger',
+    level: 3,
+    isFree: true,
+    tags: ['html','css']
+}
+let doverCastle = {
+    city: 'dover',
+    price: 300,
+    features: ['pool','stable','swings'],
+    petsAllowed: false
+}
+c(doverCastle.city,doverCastle.petsAllowed)
+
+
+
+
+
+
+
+
+
+    
